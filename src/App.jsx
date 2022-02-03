f3b0241eadccd6877d52de95e63031677cc5d0ad
ChuYeonBin <cndusqls98@gmail.com>
2022-02-03 21:17:10 +0900

    [Add] PostUploadPage 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import GlobalStyleProvider from './styles/GlobalStyleProvider';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import MainPage from './pages/MainPage/MainPage';
+import PostUploadPage from './pages/PostUploadPage/PostUploadPage';
 import ProfilePage from './pages/ProfilePage/ProfilePage';
 import RestaurantPage from './pages/RestaurantPage/RestaurantPage';
 
@@ -14,8 +15,9 @@ const App = () => {
         <Header />
         <Routes>
           <Route path="/" element={<MainPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/post/upload" element={<PostUploadPage />} />
           <Route path="/restaurant" element={<RestaurantPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
         </Routes>
         <Footer />
       </GlobalStyleProvider>
---
+++ src/pages/PostUploadPage/PostUploadPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './style';
+
+const PostUploadPage = () => {
+  return (
+    <S.PostUploadPage>
+      <h1>게시글 등록 페이지</h1>
+    </S.PostUploadPage>
+  );
+};
+
+export default PostUploadPage;
