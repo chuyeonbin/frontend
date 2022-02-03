1fba3962de505ca9da91fd955f573500cb538bca
ChuYeonBin <cndusqls98@gmail.com>
2022-02-03 19:51:39 +0900

    [Add] Header 컴포넌트 UI 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import Header from './components/Header/Header';
 
 const App = () => {
-  return <></>;
+  return <Header />;
 };
 
 export default App;
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,1 +1,62 @@
-// style.js
+import styled from 'styled-components';
+import logo from '../../images/logo.png';
+import testImg from '../../images/doji.png';
+
+export const Header = styled.header`
+  padding: 1.3rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #c5c5c5;
+`;
+
+export const MenuBar = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Menu = styled.ul`
+  display: flex;
+  list-style: none;
+`;
+
+export const MenuList = styled.li`
+  margin-right: 1rem;
+`;
+
+export const MenuButton = styled.button`
+  padding: 0.5rem;
+  font-size: 18px;
+  font-weight: bold;
+  cursor: pointer;
+  border: 1px solid #c5c5c5;
+  background: none;
+  border: none;
+`;
+
+export const Logo = styled.img.attrs({ src: `${logo}`, alt: '로고' })`
+  width: 100px;
+  height: 30px;
+`;
+
+export const LoginButton = styled.button`
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  font-weight: bold;
+  cursor: pointer;
+  border: 1px solid #c5c5c5;
+  background: none;
+`;
+
+export const UserImage = styled.img.attrs({
+  src: `${testImg}`,
+  alt: '사용자이미지',
+})`
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  cursor: pointer;
+  border: 1px solid #c5c5c5;
+  background: none;
+`;
