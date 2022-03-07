ec80f5471f7038894fe1a4d0cb2f9d8d622469b7
ChuYeonBin <cndusqls98@gmail.com>
2022-03-07 11:59:53 +0900

    [Fix] useModal hooks 전역 상태로 변경

--- src/hooks/useModal.jsx
+++ src/hooks/useModal.jsx
@@ -1,8 +1,16 @@
-import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setShowModal } from '../store/modal';
 
 const useModal = () => {
-  const [showModal, setShowModal] = useState(false);
-  const toggleModal = () => setShowModal(!showModal);
+  const showModal = useSelector(state => state.modal.visible);
+  const dispatch = useDispatch();
+
+  const toggleModal = () => {
+    dispatch(setShowModal(!showModal));
+  };
+
+  console.log(showModal);
+
   return [showModal, toggleModal];
 };
 
--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import { BrowserRouter } from 'react-router-dom';
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import user from './store/user';
+import modal from './store/modal';
 
-const reducers = combineReducers({ user });
+const reducers = combineReducers({ user, modal });
 
 export const store = configureStore({
   reducer: reducers,
