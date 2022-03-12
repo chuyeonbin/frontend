71961e0d77ec5d3174fd019a41716f6e84e4bab7
ChuYeonBin <cndusqls98@gmail.com>
2022-03-12 15:59:25 +0900

    [Add] 프로필 설정 클릭시 프로필 설정 페이지 이동

--- src/components/Header/ProfileModal/ProfileModal.jsx
+++ src/components/Header/ProfileModal/ProfileModal.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import * as S from './style';
 
 const ProfileModal = ({ toggleProfile }) => {
+  const navigate = useNavigate();
+
+  const goToPage = url => navigate(url);
+
+  const logOut = () => {};
+
   return (
     <S.ProfileModal toggle={toggleProfile}>
       <S.ProfileList>끼야아아알</S.ProfileList>
-      <S.ProfileList>프로필 설정</S.ProfileList>
+      <S.ProfileList onClick={() => goToPage('/profile')}>
+        프로필 설정
+      </S.ProfileList>
       <S.ProfileList>채팅</S.ProfileList>
       <S.ProfileList>로그아웃</S.ProfileList>
     </S.ProfileModal>
