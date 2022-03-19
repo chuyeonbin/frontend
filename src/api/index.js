588488370a3eb9c6a02fb367d717cb5e195fc3e0
ChuYeonBin <cndusqls98@gmail.com>
2022-03-19 17:23:06 +0900

    [Fix] api url 수정

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const BASE_API_URL = 'http://localhost:3001/';
-// const BASE_API_URL = 'https://api-sicksago.herokuapp.com/';
+// const BASE_API_URL = 'http://localhost:3001/';
+const BASE_API_URL = 'https://api-sicksago.herokuapp.com/';
 
 const httpInstance = axios.create({
   baseURL: BASE_API_URL,
